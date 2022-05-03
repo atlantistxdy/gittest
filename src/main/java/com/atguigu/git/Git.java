483cc4abb6dafdf05b05ee7a201ea4b0f9fdb338
Atlantis <Atlantis@atguigu.com>
2022-05-03 12:18:56 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/Git.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author Atlantis
+ * @create 2022-05-03-12:10
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git");
+    }
+
+}
